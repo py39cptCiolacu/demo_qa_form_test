46a072731842430ea983ee7e8e1f3b916189c5d7
DanielCosmin <dciolacu87@yahoo.com>
2024-11-27 18:36:43 +0200

    element_tester class

--- test_form/test_form.py
+++ test_form/test_form.py
@@ -1,29 +1,40 @@
 import pytest
 from playwright.sync_api import Page, Playwright
-from form_teste_utils import form_tester, element_tester
 
-from element_getter import ElementGetter
-from element_interactor import ElementInteractor
+from element_tester import ElementTester
 
 URL = "https://demoqa.com/automation-practice-form"
 
 
-def test_form_element(
-    page: Page, element_interactor: ElementInteractor, element_getter: ElementGetter
-) -> None:
+def test_first_name(page: Page) -> None:
 
     interact_dict = {"fill_textbox": {"locator": "#firstName", "value": "Test Test"}}
     testing_element = ("get_input_text", "#firstName")
     testing_value = "Test Test"
-    error = "Some Error"
 
-    element_tester(
+    element_tester = ElementTester(
         page=page,
-        element_interactor=element_interactor,
-        element_getter=element_getter,
         url=URL,
         interact_dict=interact_dict,
         testing_element=testing_element,
         testing_value=testing_value,
-        error=error,
     )
+
+    element_tester.test()
+
+
+def test_last_name(page: Page) -> None:
+
+    interact_dict = {"fill_textbox": {"locator": "#lastName", "value": "Test"}}
+    testing_element = ("get_input_text", "#lastName")
+    testing_value = "Test"
+
+    element_tester = ElementTester(
+        page=page,
+        url=URL,
+        interact_dict=interact_dict,
+        testing_element=testing_element,
+        testing_value=testing_value,
+    )
+
+    element_tester.test()
